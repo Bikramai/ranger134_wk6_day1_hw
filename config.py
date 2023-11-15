635c903a860a6722b1f3e18e881466e95960c3ca
bikramia <bikram.rhce@gmail.com>
2023-11-14 21:54:23 -0500

    wk6d2udate2

--- config.py
+++ config.py
@@ -3,9 +3,12 @@ from dotenv import load_dotenv #allows us to load environment variables to do ce
 
 
 
-
+# establish our base directory so whenever we use "." to reference any location in our app it knows we are referncing
+# abodezen_shop folder 
 basedir = os.path.abspath(os.path.dirname(__file__)) #this is establishing our base directory or our root folder
 
+
+#need to establish where our environment variables are coming from (this file will be hidden from github)
 load_dotenv(os.path.join(basedir, '.env')) #this is just pointing us to the direction of our environment variables (located in .env file)
 
 
@@ -21,5 +24,8 @@ class Config():
     FLASK_APP = os.environ.get('FLASK_APP') #looking for the key of Flask_APP in our .env file 
     FLASK_ENV = os.environ.get('FLASK_ENV')
     FLASK_DEBUG = os.environ.get("FLASK_DEBUG")
-    SECRET_KEY = os.environ.get('SECRET_KEY') or "Nana nana boo boo, you'll never guess this" #just needs to be present 
-  
+
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'Literally whatever you want as long as its a string. Cool Beans'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'app.db')
+
+
